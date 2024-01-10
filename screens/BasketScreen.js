fbd60f5f800a18706f4888d384274082d850f76c
Daniel Ohinoyi <bizwithdan@gmail.com>
2024-01-10 12:03:55 +0100

    completed the basket screen

--- screens/BasketScreen.js
+++ screens/BasketScreen.js
@@ -1,16 +1,20 @@
-import { useNavigation } from '@react-navigation/native';
 import React, { useMemo, useState } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity, Image, ScrollView } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { useDispatch, useSelector } from 'react-redux';
+import { XCircleIcon } from 'react-native-heroicons/solid';
 
 import { selectRestaurant } from '../features/restaurantSlice';
-import { selectBasketItems } from '../features/basketSlice';
+import { removeFromBasket, selectBasketItems, selectBasketTotal } from '../features/basketSlice';
+import { urlFor } from '../sanity';
+import formatter from '../features/numberFormatter';
 
 const BasketScreen = () => {
     const navigation = useNavigation();
     const restaurant = useSelector(selectRestaurant);
     const items = useSelector(selectBasketItems);
+    const basketTotal = useSelector(selectBasketTotal);
     const dispatch = useDispatch();
     const [groupedItemsInBasket, setGroupedItemsInBasket] = useState([]);
 
@@ -24,9 +28,91 @@ const BasketScreen = () => {
     }, [items]);
 
     return (
-        <SafeAreaView>
-            <View>
-                <Text>BasketScreen</Text>
+        <SafeAreaView className="flex-1 bg-white">
+            <View className="flex-1 bg-gray-100">
+                <View className="p-5 border-b border-[#00CCBB] bg-white shadow-xs">
+                    <View>
+                        <Text className="text-lg font-bold text-center">Basket</Text>
+                        <Text className="text-center text-gray-400">{restaurant.title}</Text>
+                    </View>
+
+                    <TouchableOpacity
+                        onPress={navigation.goBack}
+                        className="rounded-full bg-gray-100 absolute top-3 right-5"
+                    >
+                        <XCircleIcon color="#00CCBB" height={50} width={50} />
+                    </TouchableOpacity>
+                </View>
+
+                <View className="flex-row items-center space-x-4 px-4 py-3 bg-white my-5">
+                    <Image
+                        source={{
+                            uri: 'https://images.prismic.io/dbhq-deliveroo-riders-website/ed825791-0ba4-452c-b2cb-b5381067aad3_RW_hk_kit_importance.png?auto=compress,format&rect=0,0,1753,1816&w=1400&h=1450',
+                        }}
+                        className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+                    />
+
+                    <Text className="flex-1"> Deliver in 50-75 min</Text>
+                    <TouchableOpacity>
+                        <Text className="text-[#00CCBB]">Change</Text>
+                    </TouchableOpacity>
+                </View>
+
+                <ScrollView className="divide-y divide-gray-200">
+                    {Object.entries(groupedItemsInBasket).map(([key, items]) => (
+                        <View
+                            key={key}
+                            className="flex-row items-center space-x-3 bg-white py-2 px-5"
+                        >
+                            <Text className="text-[#00CCBB]">{items.length} x </Text>
+                            <Image
+                                source={{ uri: urlFor(items[0]?.image).url() }}
+                                className="h-12 w-12 rounded-full"
+                            />
+                            <Text className="flex-1">{items[0]?.name}</Text>
+
+                            <Text className="text-gray-600">
+                                {formatter(items[0]?.price)}
+                            </Text>
+
+                            <TouchableOpacity>
+                                <Text
+                                    className="text-[#00CCBB] text-xs"
+                                    onPress={() => dispatch(removeFromBasket({ id: key }))}
+                                >
+                                    Remove
+                                </Text>
+                            </TouchableOpacity>
+                        </View>
+                    ))}
+                </ScrollView>
+
+                <View className="p-5 bg-white mt-5 space-y-4">
+                    <View className="flex-row justify-between">
+                        <Text className="text-gray-400">Subtotal</Text>
+                        <Text className="text-gray-400">
+                            {formatter(basketTotal)}
+                        </Text>
+                    </View>
+
+                    <View className="flex-row justify-between">
+                        <Text className="text-gray-400">Delivery Fee</Text>
+                        <Text className="text-gray-400">
+                            {formatter(basketTotal * 0.10)}
+                        </Text>
+                    </View>
+
+                    <View className="flex-row justify-between">
+                        <Text >Order Total</Text>
+                        <Text className="font-extrabold">
+                            {formatter(basketTotal + (basketTotal * 0.10))}
+                        </Text>
+                    </View>
+
+                    <TouchableOpacity className="rounded-lg bg-[#00CCBB] p-4 ">
+                        <Text className="text-center text-white text-lg font-bold">Place Order</Text>
+                    </TouchableOpacity>
+                </View>
             </View>
         </SafeAreaView>
     )
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -25,7 +25,7 @@ const HomeScreen = () => {
         });
     }, []);
 
-    // Executes when the component load
+    // Executes when the component loads
     useEffect(() => {
         sanityClient.fetch(`
         *[_type == "featured"] {
@@ -41,61 +41,64 @@ const HomeScreen = () => {
 
 
     return (
-        <SafeAreaView className="bg-white pt-5">
-            {/** Header */}
-            <View className="flex-row pb-3 item-center justify-between mx-4 space-x-2">
-                <Image
-                    source={{
-                        uri: 'https://images.prismic.io/dbhq-deliveroo-riders-website/ed825791-0ba4-452c-b2cb-b5381067aad3_RW_hk_kit_importance.png?auto=compress,format&rect=0,0,1753,1816&w=1400&h=1450'
-                    }}
-                    className="h-7 w-7 bg-gray-300 p-4 rounded-full"
-                />
+        <SafeAreaView>
+            <View className="flex-1 bg-white pt-5">
 
-                <View className="flex-1">
-                    <Text className="font-bold text-gray-400 text-xs">
-                        Deliver Now!
-                    </Text>
-                    <Text className="font-bold text-xl">
-                        Current Location
-                        <ChevronDownIcon size={20} color="#00CCBB" />
-                    </Text>
-                </View>
 
-                <UserIcon size={35} color="#00CCBB" />
-            </View>
-
-            {/** Search */}
-            <View className="flex-row items-center space-x-2 pb-2 mx-4">
-                <View className="flex-row flex-1 space-x-2 bg-gray-200 p-3">
-                    <SearchIcon color="gray" size={20} />
-                    <TextInput
-                        placeholder="Restaurants and cuisines"
-                        keyboardType="default"
+                {/** Header */}
+                <View className="flex-row pb-3 item-center justify-between mx-4 space-x-2">
+                    <Image
+                        source={{
+                            uri: 'https://images.prismic.io/dbhq-deliveroo-riders-website/ed825791-0ba4-452c-b2cb-b5381067aad3_RW_hk_kit_importance.png?auto=compress,format&rect=0,0,1753,1816&w=1400&h=1450'
+                        }}
+                        className="h-7 w-7 bg-gray-300 p-4 rounded-full"
                     />
+
+                    <View className="flex-1">
+                        <Text className="font-bold text-gray-400 text-xs">
+                            Deliver Now!
+                        </Text>
+                        <Text className="font-bold text-xl">
+                            Current Location
+                            <ChevronDownIcon size={20} color="#00CCBB" />
+                        </Text>
+                    </View>
+
+                    <UserIcon size={35} color="#00CCBB" />
                 </View>
-                <AdjustmentsIcon color="#00CCBB" />
-            </View>
 
-            {/**body */}
-            <ScrollView className="bg-gray-100" >
-                {/** Categories */}
-                <Categories />
-
-                <View className="pb-36">
-                    {/** Featured */}
-
-                    {featuredCategories?.map(category => (
-                        <FeaturedRow
-                            key={category._id}
-                            id={category._id}
-                            title={category.name}
-                            description={category.short_description}
+                {/** Search */}
+                <View className="flex-row items-center space-x-2 pb-2 mx-4">
+                    <View className="flex-row flex-1 space-x-2 bg-gray-200 p-3">
+                        <SearchIcon color="gray" size={20} />
+                        <TextInput
+                            placeholder="Restaurants and cuisines"
+                            keyboardType="default"
                         />
-                    ))}
+                    </View>
+                    <AdjustmentsIcon color="#00CCBB" />
                 </View>
 
+                {/**body */}
+                <ScrollView className="bg-gray-100" >
+                    {/** Categories */}
+                    <Categories />
 
-            </ScrollView>
+                    <View className="pb-36">
+                        {/** Featured */}
+
+                        {featuredCategories?.map(category => (
+                            <FeaturedRow
+                                key={category._id}
+                                id={category._id}
+                                title={category.name}
+                                description={category.short_description}
+                            />
+                        ))}
+                    </View>
+
+                </ScrollView>
+            </View>
         </SafeAreaView>
     )
 }
